625784b6630f6e69f31d7605d0adb89c38979a3e
Dan Percic <danpercic86@gmail.com>
2025-02-10 10:41:30 +0200

    some changes

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -45,8 +45,9 @@ export class PricingComponent {
     protected readonly plan = signal<PricingPeriod>(PricingPeriods.month);
     protected readonly subscriptions: Subscription[] = [
         {
-            title: "Starter",
-            description: "Best option for personal use & for your next project",
+            title: "Basic",
+            description:
+                "Ideal pentru cei care doresc să facă primii pași spre o dantură sănătoasă și frumoasă, planul Basic oferă o acoperire esențială la un preț accesibil",
             pricing: {
                 [Currencies.usd]: {
                     [PricingPeriods.month]: 29,
@@ -60,7 +61,119 @@ export class PricingComponent {
             benefits: [
                 {
                     icon: check,
-                    description: "Check this",
+                    description: "O consultație gratuită pe an",
+                },
+                {
+                    icon: check,
+                    description: "Un periaj și un detartraj gratuit pe an",
+                },
+                {
+                    icon: check,
+                    description: "UN tratament de fluorizare pe an",
+                },
+                {
+                    icon: check,
+                    description: "O radiografie (retroalveolară sau panoramică) gratuită pe an",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 20% la obturații cu materiale compozite",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 10% la extracții simple",
+                },
+            ],
+            buttonLabel: "Get started",
+        },
+        {
+            title: "Standard",
+            description:
+                "Conceput pentru cei care înțeleg importanța unei danturi sănătoase și frumoase, planul Standard oferă o acoperire extinsă, incluzând tratamente mai complexe și beneficii suplimentare",
+            pricing: {
+                [Currencies.usd]: {
+                    [PricingPeriods.month]: 50,
+                    [PricingPeriods.year]: getPerYearPrice(50),
+                },
+                [Currencies.eur]: {
+                    [PricingPeriods.month]: 45,
+                    [PricingPeriods.year]: getPerYearPrice(45),
+                },
+            },
+            benefits: [
+                {
+                    icon: check,
+                    description: "3 consultații gratuite pe an",
+                },
+                {
+                    icon: check,
+                    description: "3 periaje și detartraje gratuite pe an",
+                },
+                {
+                    icon: check,
+                    description: "3 tratamente de fluorizare pe an",
+                },
+                {
+                    icon: check,
+                    description: "3 radiografii (retroalveolare sau panoramice) gratuite pe an",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 30% la obturații cu materiale compozite",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 20% la extracții simple",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 15% la tratamente endodontice",
+                },
+            ],
+            buttonLabel: "Get started",
+        },
+        {
+            title: "Premium",
+            description:
+                "Dedicat celor care își doresc tot ce este mai bun pentru sănătatea lor orală, planul Premium oferă o acoperire nelimitată și beneficii exclusive",
+            pricing: {
+                [Currencies.usd]: {
+                    [PricingPeriods.month]: 100,
+                    [PricingPeriods.year]: getPerYearPrice(100),
+                },
+                [Currencies.eur]: {
+                    [PricingPeriods.month]: 90,
+                    [PricingPeriods.year]: getPerYearPrice(90),
+                },
+            },
+            benefits: [
+                {
+                    icon: check,
+                    description: "Nelimitate consultații gratuită pe an",
+                },
+                {
+                    icon: check,
+                    description: "Nelimitate periaje și detartraje gratuit pe an",
+                },
+                {
+                    icon: check,
+                    description: "Nelimitate tratamente de fluorizare pe an",
+                },
+                {
+                    icon: check,
+                    description: "Nelimitate radiografii (retroalveolară sau panoramică) gratuite pe an",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 30% la obturații cu materiale compozite",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 20% la extracții simple",
+                },
+                {
+                    icon: check,
+                    description: "Reducere de 25% la tratamente endodontice",
                 },
             ],
             buttonLabel: "Get started",
